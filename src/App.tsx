30a1bcfe81ccba79e5823987ae3bc061a80e755c
unknown <rnrudxo2872@naver.com>
2021-12-18 00:01:07 +0900

    refresh button 뷰 수정

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,25 @@ const AssetSlider = styled(motion.section)`
   position: absolute;
 `;
 
-const RefreshSvg = styled.svg`
-  width: 200px;
-  height: 100px;
+const RefreshBtn = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+`;
+
+const RefreshWrapper = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const RefreshSvg = styled(motion.svg)`
+  width: 20px;
+  height: 20px;
+  transform-origin: 0;
 `;
 
 function App() {
@@ -58,19 +74,26 @@ function App() {
     <MainWrapper className="App">
       <GlobalStyle />
       <div style={{}}>
-        <button onClick={OnClickRefresh}>
-          <RefreshSvg
-            aria-hidden="true"
-            focusable="false"
-            role="img"
-            viewBox="0 0 512 512"
+        <RefreshBtn onClick={OnClickRefresh}>
+          <RefreshWrapper
+            whileHover={{
+              rotate: 360,
+              transition: { duration: 1, repeat: Infinity, repeatDelay: 0.3 },
+            }}
           >
-            <path
-              fill="currentColor"
-              d="M256.455 8c66.269.119 126.437 26.233 170.859 68.685l35.715-35.715C478.149 25.851 504 36.559 504 57.941V192c0 13.255-10.745 24-24 24H345.941c-21.382 0-32.09-25.851-16.971-40.971l41.75-41.75c-30.864-28.899-70.801-44.907-113.23-45.273-92.398-.798-170.283 73.977-169.484 169.442C88.764 348.009 162.184 424 256 424c41.127 0 79.997-14.678 110.629-41.556 4.743-4.161 11.906-3.908 16.368.553l39.662 39.662c4.872 4.872 4.631 12.815-.482 17.433C378.202 479.813 319.926 504 256 504 119.034 504 8.001 392.967 8 256.002 7.999 119.193 119.646 7.755 256.455 8z"
-            ></path>
-          </RefreshSvg>
-        </button>
+            <RefreshSvg
+              aria-hidden="true"
+              focusable="false"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="currentColor"
+                d="M256.455 8c66.269.119 126.437 26.233 170.859 68.685l35.715-35.715C478.149 25.851 504 36.559 504 57.941V192c0 13.255-10.745 24-24 24H345.941c-21.382 0-32.09-25.851-16.971-40.971l41.75-41.75c-30.864-28.899-70.801-44.907-113.23-45.273-92.398-.798-170.283 73.977-169.484 169.442C88.764 348.009 162.184 424 256 424c41.127 0 79.997-14.678 110.629-41.556 4.743-4.161 11.906-3.908 16.368.553l39.662 39.662c4.872 4.872 4.631 12.815-.482 17.433C378.202 479.813 319.926 504 256 504 119.034 504 8.001 392.967 8 256.002 7.999 119.193 119.646 7.755 256.455 8z"
+              ></path>
+            </RefreshSvg>
+          </RefreshWrapper>
+        </RefreshBtn>
         {isLoading ? (
           "Now Loading..."
         ) : (
